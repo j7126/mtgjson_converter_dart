b8145d9957dceb441900f7eb6f4499437bd30d6b
Jefferey Neuffer <jeffereyneuffer@gmail.com>
2024-03-14 10:07:42 +1000

    refactor move output file creation within output data model.

--- lib/all_printings.dart
+++ lib/all_printings.dart
@@ -1,54 +1,23 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:mtgjson_converter_dart/mtgjson/inputDataModel/all_sets.dart';
-import 'package:mtgjson_converter_dart/mtgjson/inputDataModel/card_set.dart';
-import 'package:mtgjson_converter_dart/mtgjson/outputDataModels/magic_life_wheel_json/all_set_cards.dart';
-import 'package:mtgjson_converter_dart/mtgjson/outputDataModels/magic_life_wheel_json/card_set.dart';
-import 'package:mtgjson_converter_dart/mtgjson/outputDataModels/magic_life_wheel_json/identifiers.dart';
+import 'package:mtgjson_converter_dart/mtgjson/outputDataModels/magic_life_wheel_json/magic_life_wheel_json.dart' as magic_life_wheel_json;
 
 void info() {
-  var file = File("./AllPrintings.json");
-  var jsonString = file.readAsStringSync();
-  Map<String, dynamic> json = jsonDecode(jsonString);
-  var allSetCards = AllSets.fromJson(json);
-  print("Date: ${allSetCards.meta.date}");
-  print("Version: ${allSetCards.meta.version}");
+  getAllSets();
 }
 
-void magicLifeWheelAllCards() {
+AllSets getAllSets() {
   var inputFile = File("./AllPrintings.json");
   var jsonString = inputFile.readAsStringSync();
   Map<String, dynamic> json = jsonDecode(jsonString);
   var allSets = AllSets.fromJson(json);
   print("Date: ${allSets.meta.date}");
   print("Version: ${allSets.meta.version}");
+  return allSets;
+}
 
-  var outputFile = File("./all_cards.json");
-
-  List<CardSet> cards = [];
-  for (var set in allSets.data.values) {
-    for (var card in set.cards) {
-      if (card.isOnlineOnly != true && card.language == "English") {
-        cards.add(card);
-      }
-    }
-  }
-
-  var outputDataModel = MagicLifeWheelJsonAllSetCards(cards
-      .map((e) => MagicLifeWheelJsonCardSet(
-            artist: e.artist,
-            identifiers: MagicLifeWheelJsonIdentifiers(
-              scryfallId: e.identifiers.scryfallId,
-              scryfallOracleId: e.identifiers.scryfallOracleId,
-              scryfallIllustrationId: e.identifiers.scryfallIllustrationId,
-            ),
-            name: e.name,
-            setCode: e.setCode,
-            uuid: e.uuid,
-          ))
-      .toList());
-
-  var result = jsonEncode(outputDataModel);
-  outputFile.writeAsStringSync(result);
+void magicLifeWheelAllCards() {
+  var allSets = getAllSets();
+  magic_life_wheel_json.magicLifeWheelAllCards(allSets);
 }
---
+++ lib/mtgjson/outputDataModels/magic_life_wheel_json/magic_life_wheel_json.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:mtgjson_converter_dart/mtgjson/inputDataModel/all_sets.dart';
+import 'package:mtgjson_converter_dart/mtgjson/inputDataModel/card_set.dart';
+import 'package:mtgjson_converter_dart/mtgjson/outputDataModels/magic_life_wheel_json/all_set_cards.dart';
+import 'package:mtgjson_converter_dart/mtgjson/outputDataModels/magic_life_wheel_json/card_set.dart';
+import 'package:mtgjson_converter_dart/mtgjson/outputDataModels/magic_life_wheel_json/identifiers.dart';
+
+void magicLifeWheelAllCards(AllSets allSets) {
+  var outputFile = File("./all_cards.json");
+
+  List<CardSet> cards = [];
+  for (var set in allSets.data.values) {
+    for (var card in set.cards) {
+      if (card.isOnlineOnly != true && card.language == "English") {
+        cards.add(card);
+      }
+    }
+  }
+
+  var outputDataModel = MagicLifeWheelJsonAllSetCards(cards
+      .map((e) => MagicLifeWheelJsonCardSet(
+            artist: e.artist,
+            identifiers: MagicLifeWheelJsonIdentifiers(
+              scryfallId: e.identifiers.scryfallId,
+              scryfallOracleId: e.identifiers.scryfallOracleId,
+              scryfallIllustrationId: e.identifiers.scryfallIllustrationId,
+            ),
+            name: e.name,
+            setCode: e.setCode,
+            uuid: e.uuid,
+          ))
+      .toList());
+
+  var result = jsonEncode(outputDataModel);
+  outputFile.writeAsStringSync(result);
+}
