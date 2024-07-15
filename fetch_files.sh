be79ae332eba13cdfa7cdfc7649ee4ce231ef4ec
Jefferey Neuffer <jeffereyneuffer@gmail.com>
2024-07-15 21:02:22 +1000

    use curl over wget for fetch files

--- fetch_files.sh
+++ fetch_files.sh
@@ -1,6 +1,6 @@
 set -e
 
 rm -f AllPrintings.json
-wget https://mtgjson.com/api/v5/AllPrintings.json
+curl -O https://mtgjson.com/api/v5/AllPrintings.json
 rm -f SetList.json
-wget https://mtgjson.com/api/v5/SetList.json
+curl -O https://mtgjson.com/api/v5/SetList.json
